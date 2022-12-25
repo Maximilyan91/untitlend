a229eb9a46b7211767eefe4d60c56fa19e0edaf3
Максим <gmax7911@gmail.com>
2022-12-25 21:44:24 +0300

    g

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person sarah = new Person("Sarah",0);
+        if (sarah.isAdult()) {
+            System.out.println(" go to Bar");
+        } else {
+            System.out.println(" go to nahoi");
+        }
+        System.out.println(sarah);
+    }
+
+
+}
+
